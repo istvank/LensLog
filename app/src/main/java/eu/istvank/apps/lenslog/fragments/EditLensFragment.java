d199623be430aa98fea5eb524a34e431a29038cd
istvank <koren@dbis.rwth-aachen.de>
2014-10-23 22:00:18 +0200

    Making the EditLensFragment updating and adding lens packs which closes #6.

--- app/src/main/java/eu/istvank/apps/lenslog/fragments/EditLensFragment.java
+++ app/src/main/java/eu/istvank/apps/lenslog/fragments/EditLensFragment.java
@@ -3,8 +3,12 @@ package eu.istvank.apps.lenslog.fragments;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
+import android.app.LoaderManager;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -28,7 +32,7 @@ import eu.istvank.apps.lenslog.provider.LensLogContract;
  * create an instance of this fragment.
  *
  */
-public class EditLensFragment extends Fragment {
+public class EditLensFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
     // the fragment initialization parameters
     private static final String ARG_LENSURI = "lensUri";
@@ -36,6 +40,14 @@ public class EditLensFragment extends Fragment {
     private Uri mLensUri;
 
     private OnFragmentInteractionListener mListener;
+
+    // views
+    private EditText mEdtLensName;
+
+    /**
+     * Identifies a particular Loader being used in this component
+     */
+    private static final int URL_LOADER = 0;
 
     /**
      * Use this factory method to create a new instance of
@@ -73,7 +85,21 @@ public class EditLensFragment extends Fragment {
         actionBar.setTitle(R.string.title_fragment_newlens);
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_lens, container, false);
+        View view = inflater.inflate(R.layout.fragment_new_lens, container, false);
+
+        // link views
+        mEdtLensName = (EditText) view.findViewById(R.id.newlens_edt_name);
+
+        // check if a new item is created or an old edited
+        if (mLensUri != null) {
+            /*
+            * Initializes the CursorLoader. The URL_LOADER value is eventually passed
+            * to onCreateLoader().
+            */
+            getLoaderManager().initLoader(URL_LOADER, null, this);
+        }
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -91,15 +117,16 @@ public class EditLensFragment extends Fragment {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.action_save) {
+            ContentValues values = new ContentValues();
+            values.put(LensLogContract.PacksColumns.EYE, "left");
+            values.put(LensLogContract.PacksColumns.LENS_TYPE, "great");
+            values.put(LensLogContract.PacksColumns.NAME, mEdtLensName.getText().toString());
+
             if (mLensUri != null) {
                 // change existing lens pack
+                getActivity().getContentResolver().update(mLensUri, values, null, null);
             } else {
                 // add new lens to database
-                EditText edtLensName = (EditText) getActivity().findViewById(R.id.newlens_edt_name);
-                ContentValues values = new ContentValues();
-                values.put(LensLogContract.PacksColumns.EYE, "left");
-                values.put(LensLogContract.PacksColumns.LENS_TYPE, "great");
-                values.put(LensLogContract.PacksColumns.NAME, edtLensName.getText().toString());
                 getActivity().getContentResolver().insert(LensLogContract.Packs.CONTENT_URI, values);
             }
 
@@ -149,4 +176,44 @@ public class EditLensFragment extends Fragment {
         public void onFragmentInteraction(Uri uri);
     }
 
+    /**
+     * Interfaces
+     */
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        /*
+        * Takes action based on the ID of the Loader that's being created
+        */
+        switch (id) {
+            case URL_LOADER:
+                // Returns a new CursorLoader
+                String[] projection = new String[] {
+                        LensLogContract.Packs._ID,
+                        LensLogContract.Packs.NAME
+                };
+                return new CursorLoader(
+                        getActivity(),  // Parent activity context
+                        mLensUri,        // Table to query
+                        projection,     // Projection to return
+                        null,           // No selection clause
+                        null,           // No selection arguments
+                        null            // Default sort order
+                );
+            default:
+                // An invalid id was passed in
+                return null;
+        }
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        data.moveToFirst();
+        mEdtLensName.setText(data.getString(data.getColumnIndexOrThrow(LensLogContract.Packs.NAME)));
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
 }
